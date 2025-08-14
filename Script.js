1c29097127be7449b02b5d64080819bac397f491
priyanga327 <priyangamaya4@gmail.com>
2025-08-14 21:57:16 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,8 @@
+// Get elements
+const navLinks = document.querySelectorAll('nav ul li a');
+const sections = document.querySelectorAll('section');
+const contactForm = document.querySelector('#contact form');
+
+// Smooth scrolling for nav links
+navLinks.forEach(link => {
+  link.add
